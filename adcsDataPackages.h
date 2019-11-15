47ce037ca1f01f7fa1c6faf2cc594902697de994
JamesSmithDal <52800013+JamesSmithDal@users.noreply.github.com>
2019-11-14 21:39:10 -0400

    Create adcsDataPackages.h
    Created a header file for all the data packages sent to and from obc as well as telemetry data. Data types are as per Telemetry Excel sheet

---
+++ adcsDataPackages.h
@@ -0,0 +1,67 @@
+#ifndef
+#define ADCSDATAPACKAGES_H
+/*
+This File contains the struct declaration for the ADCS telemetry package as well as OBC package.
+This struct contains the data that will be sent back to earth.
+*/
+
+//Struct contains Telemetry data to be sent to earth
+typedef struct adcsTelemetry {
+	//data from sun model
+	long sunMdlVX;
+	long sunMdlVY;
+	long sunMdlVZ;
+	//sun vector
+	long sunVX;
+	long sunVY;
+	long sunVZ;
+	//data for ananlog magnetometer
+	long magAVX;
+	long magAVY;
+	long magAVZ;
+	//data for digital magnetometer
+	long magDVX;
+	long magDVY;
+	long magDVZ;
+	//Data for rate gyros
+	long rateVX;
+	long rateVY;
+	long rateVZ;
+	//vector
+	long roll;
+	long pitch;
+	long yaw;
+	//state of board
+	unsigned char systemState;
+	unsigned char torquerState;
+};
+
+//Struct contains sensor data that will be sent to the OBC
+typedef struct adcsToOBC {
+	//data for ananlog magnetometer
+	long magAVX;
+	long magAVY;
+	long magAVZ;
+	//data for digital magnetometer
+	long magDVX;
+	long magDVY;
+	long magDVZ;
+	//18 photodiodes each with 12 bits
+	unsigned photodiode[18] : 12;
+	//Data for rate gyros
+	long rateVX;
+	long rateVY;
+	long rateVZ;
+	//panel temperature
+	unsigned panelTemp : 84;
+	//fault state of board
+	unsigned short faultState;
+};
+
+//Struct contains data sent to adcs board
+typedef struct obcToADCS {
+	unsigned torqueCycle : 48;
+	unsigned char torquerCmd;
+	unsigned short stateCmd;
+};
+#endif
