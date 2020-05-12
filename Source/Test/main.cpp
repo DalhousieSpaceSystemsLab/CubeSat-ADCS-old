cf936c1fdcfe5fc259559f176d239bbaa23cc965
Cathy <c.song@dal.ca>
2020-05-12 12:26:07 -0300

    Deleted unused Test File

--- Source/Test/main.cpp
+++
@@ -1,9 +0,0 @@
-//This file runs the tests for the project
-#include <gtest/gtest.h>
-#include <iostream>
-
-int main(int argc, char **argv){
-    ::testing::InitGoogleTest(&argc, argv);
-    return RUN_ALL_TESTS();
-}
-
