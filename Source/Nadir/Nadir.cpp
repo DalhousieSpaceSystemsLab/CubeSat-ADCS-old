ebd7ee9c5ce780c3b7ed41be7bebfdc5c7703385
MarkusEC <astromarkus@hotmail.com>
2020-10-25 22:23:04 -0300

    Update Nadir.cpp

--- Source/Nadir/Nadir.cpp
+++ Source/Nadir/Nadir.cpp
@@ -1,20 +1,17 @@
-
 #define _USE_MATH_DEFINES
 #include "nadir.h"
-
 Eigen::MatrixXd C1(double x) {
 	/****************************************
 	Places scalar angle x(rad) into standard
 	rotation matrix about x - axis.
-
 	input: x
 	output: C
 	****************************************/
 
 	Eigen::MatrixXd C(3, 3);
 	C << 1, 0, 0,
-		 0, cos(x), sin(x),
-		 0, -sin(x), cos(x);
+		0, cos(x), sin(x),
+		0, -sin(x), cos(x);
 	return C;
 }
 
@@ -22,15 +19,14 @@ Eigen::MatrixXd C2(double y) {
 	/****************************************
 	Places scalar angle y(rad) into standard
 	rotation matrix about y - axis.
-
 	input: y
 	output: C
 	****************************************/
-	
+
 	Eigen::MatrixXd C(3, 3);
 	C << cos(y), 0, -sin(y),
-		 0, 1, 0,
-		 sin(y), 0, cos(y);
+		0, 1, 0,
+		sin(y), 0, cos(y);
 	return C;
 }
 
@@ -38,15 +34,14 @@ Eigen::MatrixXd C3(double z) {
 	/****************************************
 	Places scalar angle z(rad) into standard
 	rotation matrix about z - axis.
-
 	input: z
 	output: C
 	****************************************/
-	
+
 	Eigen::MatrixXd C(3, 3);
 	C << cos(z), sin(z), 0,
-		 -sin(z), cos(z), 0,
-		 0, 0, 1;
+		-sin(z), cos(z), 0,
+		0, 0, 1;
 	return C;
 }
 
@@ -56,13 +51,12 @@ Eigen::MatrixXd set_C_PI(double omega, double RAAN, double i) {
 	omega = argument of perigee(rad)
 	i = inclination(rad)
 	RAAN = right ascension of the ascending node(rad)
-
 	input: omega, i, RAAN
 	output: C_PI
 	****************************************/
 
 	Eigen::MatrixXd C_PI;
-	C_PI = C3(omega)*C1(i)*C3(RAAN);
+	C_PI = C3(omega) * C1(i) * C3(RAAN);
 	return C_PI;
 }
 
@@ -72,47 +66,44 @@ Eigen::MatrixXd set_C_NPP(double tano) {
 	inertial coordinate system while NP follows the satellite. The rotation
 	is a 3 - 2 - 3 sequence through true anomaly, -90, and 90, respectively.
 	tano = true anomaly(rad).
-
 	input: tano
 	output: C_NPP
 	****************************************/
-	
+
 	Eigen::MatrixXd C_NPP;
-	C_NPP = C3(M_PI / 2)*C2(-M_PI / 2)*C3(tano);
+	C_NPP = C3(M_PI / 2) * C2(-M_PI / 2) * C3(tano);
 	return C_NPP;
 }
 
 Eigen::MatrixXd set_qq(Eigen::MatrixXd C_NPI) {
 	/****************************************
 	Sets qq matrix, the first pass of nadir quaternion coordinates.
-
 	input: C_NPI
 	output: qq
 	****************************************/
-	
+
 	Eigen::MatrixXd qq(4, 1), t(3, 3);
 	t = C_NPI;
-	
+
 	//Calculates first pass of quaternion.
-	qq << sqrt(0.25*(1 + t(0, 0) + t(1, 1) + t(2, 2))),
-		  sqrt(0.25*(1 + t(0, 0) - t(1, 1) - t(2, 2))),
-		  sqrt(0.25*(1 - t(0, 0) + t(1, 1) - t(2, 2))),
-		  sqrt(0.25*(1 - t(0, 0) - t(1, 1) + t(2, 2)));
+	qq << sqrt(0.25 * (1 + t(0, 0) + t(1, 1) + t(2, 2))),
+		sqrt(0.25 * (1 + t(0, 0) - t(1, 1) - t(2, 2))),
+		sqrt(0.25 * (1 - t(0, 0) + t(1, 1) - t(2, 2))),
+		sqrt(0.25 * (1 - t(0, 0) - t(1, 1) + t(2, 2)));
 	return qq;
 }
 Eigen::MatrixXd solve_q_NPI(Eigen::MatrixXd C_NPI) {
 	/****************************************
-	Takes the combination of rotation matricies, C_NPI, and 
+	Takes the combination of rotation matricies, C_NPI, and
 	returns quaternion for nadir.
-
 	input: C_NPI
 	output: q_NPI
 	****************************************/
-	
+
 	int idx = 0;
 	double qs, qx, qy, qz;
-	Eigen::MatrixXd q_NPI(1,4), t(3, 3), qq;
-	
+	Eigen::MatrixXd q_NPI(1, 4), t(3, 3), qq;
+
 	t = C_NPI;
 
 	//Sets first pass of quaternion.
@@ -153,29 +144,28 @@ Eigen::MatrixXd solve_q_NPI(Eigen::MatrixXd C_NPI) {
 
 	//Values for the final quaternion are initialized.
 	q_NPI << qs, qx, qy, qz;
-	
+
 	return q_NPI;
 }
 
 Eigen::MatrixXd nadir(
 	/****************************************
-	Takes node longitude(omega), right ascension of the 
+	Takes node longitude(omega), right ascension of the
 	ascending node (RAAN), inclination (i),true anomaly
 	(tano) in degrees, and returns the nadir quaternion as
- 	a 1x4 matrix (q_NPI):
-
+	a 1x4 matrix (q_NPI):
 	input: omega, RAAN, i, tano
 	output: q_NPI
 	****************************************/
-	
+
 	double omega_deg,
 	double RAAN_deg,
 	double i_deg,
-	double tano_deg) 
+	double tano_deg)
 {
 	Eigen::MatrixXd C_PI, C_NPP, C_NPI, qq(4, 1), t, q_NPI(1, 4);
 	double d2r, omega, RAAN, i, tano;
-	
+
 	//Conversion from degrees to radians.
 	d2r = M_PI / 180;
 	omega = d2r * omega_deg;
@@ -186,12 +176,12 @@ Eigen::MatrixXd nadir(
 	//Finds rotation matricies.
 	C_PI = set_C_PI(omega, RAAN, i);
 	C_NPP = set_C_NPP(tano);
-	
+
 	//Combines both rotation matricies.
 	C_NPI = C_NPP * C_PI;
-	
+
 	//Finds nadir quaternion.
 	q_NPI = solve_q_NPI(C_NPI);
-	
+
 	return q_NPI;
 }
