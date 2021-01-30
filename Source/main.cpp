10a8ea25e185be2fd83f2ee69779d6bba1faf8ce
MarkusEC <astromarkus@hotmail.com>
2021-01-30 14:48:03 -0400

    In progress.

--- Source/main.cpp
+++ Source/main.cpp
@@ -16,6 +16,7 @@
 #include "main_test.cpp"
 #include "data.h"
 #include <iostream>
+#include <string>
 
 /*
 To get a better understanding of this code, you can take a look at slides 3, 9, and 10 in the ADCS CDR presentation as well as see the high level system diagram.
@@ -29,13 +30,137 @@ void safehold();
 void standby();
 void faultDetection();
 void detumbleAndDesaturate();
-void updateEkf();
+void updateEkf(Eigen::Vector4d attitudeEstimate, EKF &ekf);
+void bodyRateCheck(const Eigen::Matrix<double, 12, 1>& rawAngRate, double &bodyRateNorm, double &rwRateNormRPM);
+void sunVecAvailabilityCheck(const Eigen::Matrix<double, 18, 1>& y, bool &sunVecAvailable, int &secondsWithoutSun);
 void useEkfPredictedState();
-void useReactionWheels();
-void zeroEkfState();
+void useReactionWheels(Eigen::Vector3d w_est, Eigen::Vector4d q_est, Eigen::Vector4d q_NPI, EKF ekf, Eigen::Vector3d &reactionWheelVoltages);
+void zeroEkfState(EKF &ekf);
+Eigen::Vector3d getAngVelFromEkf(EKF ekf);
+Eigen::Vector4d getQuatFromEkf(EKF ekf);
 
 int main()
-{   
+{  
+    std::string modeOfOperation = "standby";
+    
+    
+    
+    if(modeOfOperation == "standby"){
+        standby();  
+    }else if(modeOfOperation == "safehold"){
+        safehold();
+    }
+}
+
+//Modes of operation (slide 3)
+
+void leop(){
+}
+
+void safehold(){
+}
+
+void faultDetection(){
+}
+
+//Controller logic flow functions to be implemented for standby mode (see slide 10 on ADCS CDR presentation)
+
+void detumbleAndDesaturate(){
+}
+
+Eigen::Vector3d getAngVelFromEkf(EKF ekf){
+    Eigen::Vector3d w_est;
+    w_est[0] = ekf.get_x_hat_kk()(3);
+    w_est[1] = ekf.get_x_hat_kk()(4);
+    w_est[2] = ekf.get_x_hat_kk()(5);
+    return w_est;
+}
+
+Eigen::Vector4d getQuatFromEkf(EKF ekf){
+    Eigen::Vector4d q_est;
+    q_est[0] = ekf.get_x_hat_kk()(9); // scalar was to the right of vector components in EKF.
+    q_est[1] = ekf.get_x_hat_kk()(6);
+    q_est[2] = ekf.get_x_hat_kk()(7);
+    q_est[3] = ekf.get_x_hat_kk()(8);
+    return q_est;
+}
+
+void updateEkf(Eigen::Vector4d attitudeEstimate, EKF &ekf){
+    Eigen::Matrix<double, 6, 1> u = Eigen::Matrix<double, 6, 1>::Zero();
+    Eigen::Matrix<double, 10, 1> y1 = Eigen::Matrix<double, 10, 1>::Zero();
+    
+    y1[0] = s.rawAngRate(0);        //ws1 scalar angular speed of RW#1 relative to BF
+    y1[1] = s.rawAngRate(1);        //ws2 scalar angular speed of RW#2 relative to BF
+    y1[2] = s.rawAngRate(2);        //ws3 scalar angular speed of RW#3 relative to BF
+    y1[3] = s.rawAngRate(3);        //wx CubeSat angular velocity relative to ECI expressed in BF (x component) rad/s
+    y1[4] = s.rawAngRate(4);        //wy CubeSat angular velocity relative to ECI expressed in BF (y component) rad/s
+    y1[5] = s.rawAngRate(5);        //wz CubeSat angular velocity relative to ECI expressed in BF (z component) rad/s
+    y1[6] = attitudeEstimate(1);    //q1 element 1 of vector portion of quaternion representing rotation from ECI to BF
+    y1[7] = attitudeEstimate(2);    //q2 element 2 of vector portion of quaternion representing rotation from ECI to BF
+    y1[8] = attitudeEstimate(3);    //q3 element 3 of vector portion of quaternion representing rotation from ECI to BF
+    y1[9] = attitudeEstimate(0);    //q0 scalar element quaternion representing rotation from ECI to BF
+
+    u[0] = s.rawAngRate(6);         //gx applied external torque x (in BF) Nm
+    u[1] = s.rawAngRate(7);         //gy applied external torque y (in BF) Nm
+    u[2] = s.rawAngRate(8);         //gz applied external torque z (in BF) Nm
+    u[3] = s.rawAngRate(9);         //ga1 applied scalar torque from motor on RW#1 Nm
+    u[4] = s.rawAngRate(10);        //ga2 applied scalar torque from motor on RW#2 Nm
+    u[5] = s.rawAngRate(11);        //ga3 applied scalar torque from motor on RW#3 Nm
+    
+    ekf.update(u, y1);
+}
+void bodyRateCheck(const Eigen::Matrix<double, 12, 1>& rawAngRate, double &bodyRateNorm, double &rwRateNormRPM){
+Eigen::Vector3d bodyRate, rwRate;
+bodyRate(0) = rawAngRate(3);
+bodyRate(1) = rawAngRate(4);
+bodyRate(2) = rawAngRate(5);
+
+bodyRateNorm = bodyRate.norm();
+
+rwRate(0) = rawAngRate(0);
+rwRate(1) = rawAngRate(1);
+rwRate(2) = rawAngRate(2);
+
+rwRateNormRPM = rwRate.norm()*60/(2*M_PI);
+}
+void sunVecAvailabilityCheck(const Eigen::Matrix<double, 18, 1>& y, bool &sunVecAvailable, int &secondsWithoutSun){
+    /*
+    If more than 15 of the 18 sun sensors are returning values under the threshold of 0.5, the LORIS is considered to be in an eclipse.
+    this function returns true if in eclipse, and false if not in eclipse.
+    */
+   int darkenedSensors = 0;
+    for (int i = 0; i < 18; i++){
+        if (y(i) < 0.5){
+            darkenedSensors++;
+        }
+    }
+    if (darkenedSensors > 15){
+        sunVecAvailable = false;
+        secondsWithoutSun++;
+    }else{
+        sunVecAvailable = true;
+        secondsWithoutSun = 0;
+    }
+}
+
+void useReactionWheels(Eigen::Vector3d w_est, Eigen::Vector4d q_est, Eigen::Vector4d q_NPI, EKF ekf, Eigen::Vector3d &reactionWheelVoltages){
+            w_est = getAngVelFromEkf(ekf);
+            q_est = getQuatFromEkf(ekf);
+            reactionWheelVoltages = pd(w_est, q_est, q_NPI);
+}
+
+void zeroEkfState(EKF &ekf){
+    ekf.set_x_hat_kk(Eigen::Matrix<double, 13, 1>::Zero());
+    ekf.set_P_kk(Eigen::Matrix<double, 13, 13>::Zero());
+
+}
+void standby(){
+bool sunVecAvailable = false;
+    bool desaturate = false;
+    int secondsWithoutSun;
+    double bodyRateNorm;
+    double rwRateNormRPM;
+    
     //Initializing the EKF with the initial values provided by Dr. Bauer's Matlab code.
     Eigen::Matrix<double, 13, 1> x_hat_kk_initial = Eigen::Matrix<double, 13, 1>::Zero();
     x_hat_kk_initial << -.02, .04, .01, -.01, .01, .002, .5, -.25, .3, .8, .1, -.1, .15;
@@ -69,74 +194,34 @@ int main()
 
     //These values are passed to the q method and the attitude estimate quaternion is returned.
     Eigen::Vector4d attitudeEstimate = q_method(b1, b2, b3, r1, r2);
-
-    Eigen::Matrix<double, 6, 1> u = Eigen::Matrix<double, 6, 1>::Zero();
-    Eigen::Matrix<double, 10, 1> y1 = Eigen::Matrix<double, 10, 1>::Zero();
     
-    y1[0] = s.rawAngRate(0);        //ws1 scalar angular speed of RW#1 relative to BF
-    y1[1] = s.rawAngRate(1);        //ws2 scalar angular speed of RW#2 relative to BF
-    y1[2] = s.rawAngRate(2);        //ws3 scalar angular speed of RW#3 relative to BF
-    y1[3] = s.rawAngRate(3);        //wx CubeSat angular velocity relative to ECI expressed in BF (x component) rad/s
-    y1[4] = s.rawAngRate(4);        //wy CubeSat angular velocity relative to ECI expressed in BF (y component) rad/s
-    y1[5] = s.rawAngRate(5);        //wz CubeSat angular velocity relative to ECI expressed in BF (z component) rad/s
-    y1[6] = attitudeEstimate(1);    //q1 element 1 of vector portion of quaternion representing rotation from ECI to BF
-    y1[7] = attitudeEstimate(2);    //q2 element 2 of vector portion of quaternion representing rotation from ECI to BF
-    y1[8] = attitudeEstimate(3);    //q3 element 3 of vector portion of quaternion representing rotation from ECI to BF
-    y1[9] = attitudeEstimate(0);    //q0 scalar element quaternion representing rotation from ECI to BF
-
-    u[0] = s.rawAngRate(6);         //gx applied external torque x (in BF) Nm
-    u[1] = s.rawAngRate(7);         //gy applied external torque y (in BF) Nm
-    u[2] = s.rawAngRate(8);         //gz applied external torque z (in BF) Nm
-    u[3] = s.rawAngRate(9);         //ga1 applied scalar torque from motor on RW#1 Nm
-    u[4] = s.rawAngRate(10);        //ga2 applied scalar torque from motor on RW#2 Nm
-    u[5] = s.rawAngRate(11);        //ga3 applied scalar torque from motor on RW#3 Nm
-    
-    ekf.update(u, y1);
-
-    Eigen::Vector3d w_est;
-    w_est[0] = ekf.get_x_hat_kk()(3);
-    w_est[1] = ekf.get_x_hat_kk()(4);
-    w_est[2] = ekf.get_x_hat_kk()(5);
-
-    Eigen::Vector4d q_est;
-    q_est[0] = ekf.get_x_hat_kk()(9); // scalar was to the right of vector components in EKF.
-    q_est[1] = ekf.get_x_hat_kk()(6);
-    q_est[2] = ekf.get_x_hat_kk()(7);
-    q_est[3] = ekf.get_x_hat_kk()(8);
-
     Eigen::Vector4d q_NPI_transpose = q_NPI.transpose();// Nadir algorithm needs to return 4x1 instead of 1x4.
+    Eigen::Vector3d reactionWheelVoltages;
+    Eigen::Vector3d w_est;
+    Eigen::Vector4d q_est;
 
-    std::cout << pd(w_est, q_est, q_NPI_transpose); //Printing the voltages in BF to go to MSP430.
+    bodyRateCheck(s.rawAngRate, bodyRateNorm, rwRateNormRPM);
+    sunVecAvailabilityCheck(s.rssd.y, sunVecAvailable, secondsWithoutSun);
+
+if(bodyRateNorm > 0.04 || rwRateNormRPM > 5000.0){
+    desaturate = true;
+    reactionWheelVoltages = Eigen::Vector3d::Zero(); //Do RW voltages need to be zeroed?
+    //use Bdot to calculate magnetic torques for magnetorquers (detumble).
+}else{
+  if(sunVecAvailable){ 
+        updateEkf(attitudeEstimate, ekf);               
+        useReactionWheels(w_est, q_est, q_NPI_transpose, ekf, reactionWheelVoltages);
+
+    }else{
+        if(secondsWithoutSun < 300.0){
+            //Not updating ekf, using its last predicted state.    
+            useReactionWheels(w_est, q_est, q_NPI_transpose, ekf, reactionWheelVoltages);
+        }else{
+            zeroEkfState(ekf);
+            reactionWheelVoltages = Eigen::Vector3d::Zero(); //Do RW voltages need to be zeroed?
+            //use Bdot to calculate magnetic torques for magnetorquers (detumble).
+        }        
+    }  
 }
-
-//Modes of operation (slide 3)
-
-void leop(){
-}
-
-void safehold(){
-}
-
-void standby(){
-}
-
-void faultDetection(){
-}
-
-
-//Controller logic flow functions to be implemented for standby mode (see slide 10 on ADCS CDR presentation)
-
-void detumbleAndDesaturate(){
-}
-
-void updateEkf(){
-}
-
-void useEkfPredictedState(){
-}
-
-void useReactionWheels(){
-}
-
-void zeroEkfState(){
+    std::cout << reactionWheelVoltages; //Printing the voltages in BF to go to MSP430.
 }
