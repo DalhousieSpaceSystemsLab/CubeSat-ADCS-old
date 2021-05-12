2f5ec02be2c5fef505e1734de627625970a658be
Rutwij Makwana <rutwij@dal.ca>
2021-05-12 19:09:44 -0300

    Added main.h

--- Source/main.cpp
+++ Source/main.cpp
@@ -3,36 +3,38 @@
 #include <thread>
 
 #include "CubeSat_ADCSConfig.h"
+#include "main.h"
 #include "JSON.h"
 #include "UART.h"
 
 using namespace std;
 
-std::string json_test = "{\"name\" : \"Jack\", \"age\" : 27, \"age\" : 27, \"age\" : 27, \"age\" : 27, \"age\" : 27}";
-
-std::string json_test2 = "{\"menu\": { \"id\": \"file\", \"value\": \"File\", \"popup\": {\"menuitem\": [{\"value\": \"New\", \"onclick\": \"CreateNewDoc()\"}, {\"value2\": \"Open\", \"onclick\": \"OpenDoc()\"}, {\"value3\": \"Close\", \"onclick\": \"CloseDoc()\"}]}}}";
-
-std::string json_test3 = "{\"menu\": {\"id\": -123.2345, \"value\": false, \"popup\": {\"menuitem\": {\"key1\": \"value1\", \"key2\": {\"key3\": \"value3\"}}}}}";
-
 void handle_uart();
 
 int main() {
     
+    // create uart thread and main processing thread
     std::thread uart_thread(handle_uart);
+    std::thread main_thread(handle_main);
+    std::thread watchdog_thread(handle_watchdog);
+    std::thread logging_thread(handle_logging);
     
     while(true) {
-        cout << "Hello from main" << endl;
-        sleep(3.14159);
+        
     }
     
     uart_thread.join();
-    return 0;
+    main_thread.join();
+    watchdog_thread.join();
+    logging_thread.join();
+
+    return SUCCESS;
 }
 
 void handle_uart() {
-    UART ser = UART("/home/oem/com1");
+    UART ser = UART(UART_DEVICE);
     cout << ser.getDeviceName() << endl;
-    ret_val r = ser.begin(57600);
+    ret_val r = ser.begin(UART_BAUDRATE);
     cout << r << endl;
 
     if(r == SUCCESS) {
